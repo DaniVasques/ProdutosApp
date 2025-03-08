560fc28be16a5f791f8c18a642e3c173f573ce57
DaniVasques <vasques.dani13@gmail.com>
2025-03-08 18:10:57 -0300

    first commit

---
+++ ProdutosApp/Controllers/CategoriasController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProdutosApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ ProdutosApp/Controllers/ProdutosController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProdutosApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        /// <summary>
+        /// Serviços de cadsatro de produtos
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Serviços para atualização de produtos na API.
+        /// </summary>
+        /// <returns></returns>
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Serviços para exclusão de produtos na API.
+        /// </summary>
+        /// <returns></returns>
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Serviços para consulta de produtos na API.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+
+    }
+}
